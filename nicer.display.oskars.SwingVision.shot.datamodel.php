48c2affbb9fa895b951470d73e437de44079f663
Richard Rouse <rjdrouse@htsresources.com>
2023-02-17 21:36:25 -0800

    Update nicer.display.oskars.SwingVision.shot.datamodel.php

--- nicer.display.oskars.SwingVision.shot.datamodel.php
+++ nicer.display.oskars.SwingVision.shot.datamodel.php
@@ -1,19 +1,3 @@
-<b> Oskar's SwingVision Data Model API</b>
-<br>
-If you export the .xlsx file out of the SwingVision app on your phone, this trick allows you to analyze the data using a webpage. Now you can do some good graphs and take specific snippets from the video (still need to figure that out). 
-<br><br>
-First you need to convert the .xlsx file into csv files you do it by running the python package, xlsx2csv, (need to install using pip) like so:
-<br>
-<pre>xlsx2csv -s 2 SwingVision-Ball_Machine_Practice-2023-02-12_at_22.46.02.xlsx shots_file.csv</pre>
-Then you dump this script on a webserver that runs PHP, this php code converts the csv into a JSON type format and these functions are used to do the number crunching which is now easy enough to graph it up. 
-<br><br>
-An example of this code is posted at: http://boxa.labbot3dsystems.com/tennisdata/data.org.php
-<br><br>
-If you like this show some love and use our referral code if you still haven't up for SwingVision:<br>
-<a href=>https://swing.tennis/r/ee4778c442f60bcf>https://swing.tennis/r/ee4778c442f60bcf</a>
-<br><br>
-<a href=https://swing.tennis/matches/sw2-Nwjhejk>https://swing.tennis/matches/sw2-Nwjhejk</a>
-<br><br>
 <?
 $rallies = ralliesdata();
 $shots = shotsdata();
